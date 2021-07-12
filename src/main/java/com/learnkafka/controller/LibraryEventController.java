ceac529a420c457b2f7064d5478a9a7a2321f162
Alfred Xiao <alfred.xiao@qantashotels.com>
2021-07-12 10:52:35 +1000

    added producerrecord approach

--- src/main/java/com/learnkafka/controller/LibraryEventController.java
+++ src/main/java/com/learnkafka/controller/LibraryEventController.java
@@ -7,12 +7,12 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.kafka.support.SendResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeoutException;
 
 @RestController
 @Slf4j
@@ -22,13 +22,14 @@ public class LibraryEventController {
     LibraryEventProducer libraryEventProducer;
 
     @PostMapping("/v1/libraryevent")
-    public ResponseEntity<LibraryEvent> postLibraryEvent(@RequestBody LibraryEvent libraryEvent) throws JsonProcessingException, ExecutionException, InterruptedException {
+    public ResponseEntity<LibraryEvent> postLibraryEvent(@RequestBody LibraryEvent libraryEvent) throws JsonProcessingException, ExecutionException, InterruptedException, TimeoutException {
         // invoke kafka
         log.info("Before sendLibraryEvent");
 //        libraryEventProducer.sendLibraryEvent(libraryEvent);
 
-        SendResult<Integer, String> sendResult = libraryEventProducer.sendLibraryEventSynchronous(libraryEvent);
-        log.info("Send Result is {}", sendResult.toString());
+//        SendResult<Integer, String> sendResult = libraryEventProducer.sendLibraryEventSynchronous(libraryEvent);
+        libraryEventProducer.sendLibraryEventAsProducerRecord(libraryEvent);
+//        log.info("Send Result is {}", sendResult.toString());
 
         log.info("After sendLibraryEvent");
 
